6242827488420b0340661621036dc231e1375de9
Prasanna Venkatesh S <prasanna2944@gmail.com>
2025-10-10 10:01:40 +0530

    Fix: Imported createUserOrder into Inngest API route

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,14 @@
+// app/api/inngest/route.js
+
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+import { 
+    inngest, 
+    syncUserCreation, 
+    syncUserDeletion, 
+    syncUserUpdation, 
+    createUserOrder // <--- ADD THIS FUNCTION NAME
+} from "@/config/inngest"; 
+
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
@@ -8,6 +17,6 @@ export const { GET, POST, PUT } = serve({
     syncUserCreation,
     syncUserUpdation,
     syncUserDeletion,
-    createUserOrder
+    createUserOrder // Now it is correctly defined!
   ],
 });
